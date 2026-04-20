392b4fbbf36685a77cc1e21116d496382d3c8df7
eissahasn07-alt <eissahasn07@gmail.com>
2026-04-21 00:46:43 +0200

    Add files via upload

---
+++ lib/   main.dart   app.dart   screens/   services/ai.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AIScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("NileAI")),
+      body: Center(child: Text("اسأل AI")),
+    );
+  }
+}
---
+++ lib/   main.dart   app.dart   screens/   services/products.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ProductsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("المنتجات")),
+      body: Center(child: Text("قائمة المنتجات")),
+    );
+  }
+}
