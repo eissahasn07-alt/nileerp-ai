b174e12b0ec85ea2d7a5abb4de000fa88bf3e84b
eissahasn07-alt <eissahasn07@gmail.com>
2026-04-21 00:43:17 +0200

    Add files via upload

---
+++ lib/   main.dart   app.dart   screens/   services/   core/theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData dark() {
+    return ThemeData(
+      brightness: Brightness.dark,
+      scaffoldBackgroundColor: Color(0xFF0f172a),
+      primaryColor: Color(0xFFF5C542),
+    );
+  }
+}
